bf461bd83faecf0722ed89d8d4c98318dafb6cae
Peter Hickman <peter@abelsoninfo.com>
2017-07-31 14:00:17 +0100

    how this ever worked

--- db_sizes.sh
+++ db_sizes.sh
@@ -2,7 +2,7 @@
 
 for DB in `psql -l | grep '|' | awk '{print $1}' | egrep -v '(Name|template0|template1|postgres|\|)'`
 do
-    COUNT=`psql -c "SELECT pg_size_pretty(pg_database_size('$DB'));" | head -3 | tail -1`
+    COUNT=`psql $DB -c "SELECT pg_size_pretty(pg_database_size('$DB'));" | head -3 | tail -1`
     printf "Database ..: %-30s %12s\n" $DB "$COUNT"
     for TABLE in `psql $DB -c '\dt' | awk '/table/{print $3}'`
     do
